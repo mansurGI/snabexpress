192dde85baf87fcb8b3944a54ef9de5820aa36aa
yabloko18 <73183432+yabloko18@users.noreply.github.com>
2021-03-06 22:01:19 +0400

    Доделана главная страница

--- Main page/js/script.js
+++ Main page/js/script.js
@@ -1,10 +1,4 @@
-// Slider
-let swiper = new Swiper('.swiper-container', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
+
 
 // Burger
 let burgerButton = document.querySelector('.burger__body'),
@@ -16,3 +10,38 @@ burgerButton.onclick = function () {
     menuList.classList.toggle('active');
     bodyLock.classList.toggle('lock');
 }
+// Header
+let prevNav = document.querySelector('.body-header');
+window.addEventListener('scroll', function() {
+    if (pageYOffset > 85) {
+        prevNav.classList.add('transform');
+    } else {
+        prevNav.classList.remove('transform');
+    }
+});
+// First slider
+let swiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+// Second slider
+let secondSwiper = new Swiper('.second-swiper', {
+    navigation: {
+        nextEl: '.next-btn',
+        prevEl: '.prev-btn',
+    },
+    slidesPerView: '2',
+    spaceBetween: 20,
+    breakpoints: {
+        200: {
+            slidesPerView: 1,
+            spaceBetween: 10
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 10
+        },
+    }
+});
