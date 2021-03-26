7e641c80705c8675c2cfa5d286794459e139e10f
Mansur <vipgmi05@gmail.com>
2021-03-26 14:01:27 +0300

    new workspace

---
+++ script/script.js
@@ -0,0 +1,43 @@
+// Fixed Header
+let prevNav = document.querySelector('.header__pc');
+window.addEventListener('scroll', function () {
+    if (pageYOffset > 171) {
+        prevNav.classList.add('transform');
+    } else {
+        prevNav.classList.remove('transform');
+    }
+});
+
+
+// Burger
+function burger (burgerClass, mobileListClass) {
+    let bodyLock = document.querySelector('body'),
+        burgerBody = document.querySelector(burgerClass),
+        mobileList = document.querySelector(mobileListClass);
+
+
+    burgerBody.onclick = function () {
+        burgerBody.classList.toggle('active');
+        mobileList.classList.toggle('active');
+        bodyLock.classList.toggle('lock');
+    }
+}
+burger('.burger__body', '.header__bottom');
+
+
+// Map
+let map;
+
+function initMap() {
+    "use strict";
+    let pos = new google.maps.LatLng(52.51663, 13.38978);
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: pos,
+        zoom: 16,
+    });
+    let marker = new google.maps.Marker({
+        position: pos,
+        map: map,
+        icon: 'image/icons/map-marker.png',
+    });
+}
