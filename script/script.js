a65a1d020a412f0023b785720687d35e4dd640d0
Mansur <vipgmi05@gmail.com>
2021-03-26 19:43:09 +0300

    main page moved

--- script/script.js
+++ script/script.js
@@ -41,3 +41,35 @@ function initMap() {
         icon: 'image/icons/map-marker.png',
     });
 }
+
+// Fullscreen-slider
+let swiper = new Swiper('.fullscreen-slider', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+// Second slider
+let secondSwiper = new Swiper('.reviews-slider', {
+    navigation: {
+        nextEl: '.next-btn',
+        prevEl: '.prev-btn',
+    },
+    pagination: {
+        el: '.swiper-pagination_second',
+        clickable: true,
+    },
+    slidesPerView: '2',
+    spaceBetween: 20,
+    breakpoints: {
+        200: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        1200: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+    }
+});
